b8a4006b1d29766388ec85a9d2d725f75fbed529
Sthefanny Gonzaga <sthefannygonzaga@gmail.com>
2025-01-01 15:03:18 -0300

    copy

--- api/generate-hash.js
+++ api/generate-hash.js
@@ -1,16 +1,30 @@
 const crypto = require('crypto');
 
 export default function handler(req, res) {
-    const secretKey = process.env.SECRET_KEY; // Obtém a chave secreta da variável de ambiente
-    const { visitorId } = req.query;
+    console.log("Iniciando geração do hash...");
 
+
+    const secretKey = "eb0347177563df5578f30e4c23bc2717d6ce09f0";
+    if (!secretKey) {
+        console.error("SECRET_KEY não configurada");
+        return res.status(500).json({ error: "SECRET_KEY is not set" });
+    }
+
+    const { visitorId } = req.query;
     if (!visitorId) {
+        console.error("Visitor ID não fornecido");
         return res.status(400).json({ error: "Visitor ID is required" });
     }
 
-    const hash = crypto.createHmac('sha256', secretKey)
-        .update(visitorId)
-        .digest('hex');
+    try {
+        const hash = crypto.createHmac('sha256', secretKey)
+            .update(visitorId)
+            .digest('hex');
 
-    res.status(200).json({ hash });
+        console.log("Hash gerado com sucesso:", hash);
+        res.status(200).json({ hash });
+    } catch (error) {
+        console.error("Erro ao gerar hash:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 }
